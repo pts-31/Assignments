91297bcd539dd4f7db4491ccb95b6a29d3a521c1
pts-31 <51888041+pts-31@users.noreply.github.com>
2019-06-19 23:05:02 +0530

    Serialization added

---
+++ FileIO and Exception Handling/FileIO/serialization/User.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 7829136421241571165L;
+    
+    private String firstName;
+    private String lastName;
+    private int accountNumber;
+    private Date dateOpened;
+ 
+    public User(String firstName, String lastName, int accountNumber, Date dateOpened) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+        this.dateOpened = dateOpened;
+    }
+     
+    public User() {
+        super();
+    }
+ 
+    public final String getFirstName() {
+        return firstName;
+    }
+ 
+    public final String getLastName() {
+        return lastName;
+    }
+ 
+    public final int getAccountNumber() {
+        return accountNumber;
+    }
+ 
+    public final Date getDateOpened() {
+        return new Date(dateOpened.getTime());
+    }
+ 
+    public final void setFirstName(String aNewFirstName) {
+        firstName = aNewFirstName;
+    }
+ 
+    public final void setLastName(String aNewLastName) {
+        lastName = aNewLastName;
+    }
+ 
+    public final void setAccountNumber(int aNewAccountNumber) {
+        accountNumber = aNewAccountNumber;
+    }
+ 
+    public final void setDateOpened(Date aNewDate) {
+        Date newDate = new Date(aNewDate.getTime());
+        dateOpened = newDate;
+    }
+ 
+    //Setters and Getters
+ 
+    private void readObject(ObjectInputStream aInputStream) throws ClassNotFoundException, IOException
+    {      
+        firstName = aInputStream.readUTF();
+        lastName = aInputStream.readUTF();
+        accountNumber = aInputStream.readInt();
+        dateOpened = new Date(aInputStream.readLong());
+    }
+ 
+    private void writeObject(ObjectOutputStream aOutputStream) throws IOException
+    {
+        aOutputStream.writeUTF(firstName);
+        aOutputStream.writeUTF(lastName);
+        aOutputStream.writeInt(accountNumber);
+        aOutputStream.writeLong(dateOpened.getTime());
+    }
+}
